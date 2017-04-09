161631e77e2d133222ed078bf74e82bfceac9f8c
Damyan Kalev <dzkalev@gmail.com>
2017-04-09 19:33:30 +0100

    Fixed random board issue

--- src/Color.java
+++ src/Color.java
@@ -13,15 +13,5 @@ public enum Color {
 	YELLOW,
 	GREEN,
 	WHITE,
-	BLACK;
-	
-	private static final List<Color> values = Collections.unmodifiableList(Arrays.asList(values()));
-	private static final int SIZE = values.size();
-	private static final Random RANDOM = new Random();
-
-	public static Color getRandomColor(){
-		return values.get(RANDOM.nextInt(SIZE-2));
-	}
-	
-	
+	BLACK
 }
